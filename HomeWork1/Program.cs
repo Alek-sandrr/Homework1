a3c6b70b2ba44ca2f1b1f4192eccab51c1ecb271
Chernykh Aleksandr <dernet4@gmail.com>
2025-02-23 16:36:00 +0300

    Task1

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        //Дано трехзначное число. Обнулить в нем разряд десятков.
+        static int nullten(int x) => x / 100 * 100 + x % 10;
+        //LOG1
+        //Введите число:123
+        //новое число = 103
+        static void Main(string[] args)
+        {
+            //Console.Write("Введите число:");
+            //int x =Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"новое число={nullten(x)}");
+        }
+    }
+}
